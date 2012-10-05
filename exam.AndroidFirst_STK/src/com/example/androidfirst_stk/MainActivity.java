0287f1bedf0d5b3b6a27f89219746afc7ec94909
icaruswings <snowthing@naver.com>
2012-10-05 17:24:01 +0900

    MainActivity

---
+++ exam.AndroidFirst_STK/src/com/example/androidfirst_stk/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.androidfirst_stk;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView MyText = new TextView(this);
+        setContentView(MyText);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+}
